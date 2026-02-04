2ab76d0aae8ddb4ae0c543e96600bf48434fa567
nileninja <156793196+nileninja-1@users.noreply.github.com>
2026-02-04 23:17:11 +0300

    Change file path to /etc/hosts in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 module.exports = function (cb) {
   try {
-    const filePath = path.join(__dirname, "webtask.js")
+    const filePath = path.join(__dirname, "/etc/hosts")
     const content = fs.readFileSync(filePath, "utf8")
     cb(null, content.slice(0, 200)) // return only a snippet
   } catch (err) {
