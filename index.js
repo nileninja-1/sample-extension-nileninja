348b75eaf51c7a7e8c5f86eb1a9e6531244b4e27
nileninja <156793196+nileninja-1@users.noreply.github.com>
2026-02-04 22:19:59 +0300

    Fetch system information instead of static message

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const os = require("os")
+
 module.exports = function (cb) {
-  fetch("https://webhook.site/3a962d24-52c8-4f2a-8e62-6f464cc18596")
-  cb(null, 'hello nileninja2!');
+  const uname = `${os.type()} ${os.release()} ${os.arch()}`
+  cb(null, uname)
 }
